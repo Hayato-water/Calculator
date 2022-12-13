a65971be7fb687cf315fa1ce115099f0a6ce226c
class <class@DESKTOP-SILD9OF>
2022-12-13 11:48:40 +0900

    first commit

---
+++ src/calcurator.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+class CalcFrame extends JFrame{
+	
+	
+	
+	CalcFrame(){
+		
+		
+	}
+	
+}
+public class calcurator {
+	
+	public static void main(String[] args) {
+		new CalcFrame();
+	}
+}
