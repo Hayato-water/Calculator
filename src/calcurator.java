23574580a3278188929993b2bf2766b7a49adcfd
class <class@DESKTOP-SILD9OF>
2022-12-15 09:24:12 +0900

    12/14授業終了時点

--- src/calcurator.java
+++ src/calcurator.java
@@ -15,6 +15,9 @@ class CalcFrame extends JFrame implements ActionListener{
 	JButton[] button = new JButton[20]; //ボタンを格納する配列
 	JPanel panel = new JPanel(); //ボタンを乗せるパネル
 	String str = ""; //数字を文字列として格納する変数
+	String s; //計算する際、演算子を格納する
+	double d1; //計算する際、数値を格納する
+	double d2; //計算する際、数値を格納する
 	double operand = 0; //文字列から数字に変換して格納する変数
 	ArrayList<String> box = new ArrayList<>(); //数字等を文字列として先入れ後出しする箱
 	ArrayList<String> subBox = new ArrayList<>(); //補助用の箱。演算子を一時保管する
@@ -78,50 +81,174 @@ class CalcFrame extends JFrame implements ActionListener{
 				//押されたボタンの数字等をラベルに表示する
 				if(i != 16) { 
 					label.setText(label.getText() + button[i].getText());
+					System.out.println("押されたキーは" + button[i].getText());
 				}
 				//数字または小数点が押され続ける限り、それを文字列として格納する
 				if(0 <= i && i <= 10) { 
-					str = str + button[i].getText();
-					 //文字列が小数点のみなら先頭に0を付ける
+					//文字列が小数点のみなら先頭に0を付ける
 					if(str == ".") {
 						str = 0 + str;
 					}
+					str = str + button[i].getText();
 					//小数点は一回しか押せない
 					if(i == 10) { 
 						button[10].setEnabled(false);
 					}
+					System.out.println("strは" + str);
 				}
 				//イコールを押したら答えを表示する。すべての変数をリセットする
-				if(i == 12) {
-					label.setText(label.getText() + button[i].getText() + operand);
-					System.out.println(operand);
-					str = "";
-					operand = 0;
-					box.clear();
-					subBox.clear();
+				else if(i == 11) {
+					//計算する
+					if(str != "") {//strが空でない必要がある
+						box.add(str);
+						box.add(subBox.get(subBox.size()-1));
+						subBox.remove(subBox.size()-1);
+						d1 = Double.parseDouble(box.get(box.size()-3));
+						d2 = Double.parseDouble(box.get(box.size()-2));
+						s = box.get(box.size()-1);
+						box.remove(box.size()-1);
+						box.remove(box.size()-1);
+						box.remove(box.size()-1);
+						System.out.println("d1は" + d1 + "、d2は" + d2 + "、sは" + s);
+						operand = calc(d1, d2, s);
+						if(subBox.size() > 0) {
+							System.out.println("operandは" + operand);
+							str = String.valueOf(operand);
+							box.add(str);
+							box.add(subBox.get(subBox.size()-1));
+							d1 = Double.parseDouble(box.get(box.size()-3));
+							d2 = Double.parseDouble(box.get(box.size()-2));
+							s = box.get(box.size()-1);
+							System.out.println("d1は" + d1 + "、d2は" + d2 + "、sは" + s);
+							operand = calc(d1, d2, s);
+						}
+						//答えを表示
+						label.setText(label.getText() + operand);
+						System.out.println("答えは" + operand);
+						//変数リセット
+						str = "";
+						operand = 0;
+						box.clear();
+						subBox.clear();
+						button[10].setEnabled(true);
+					}
 				}
 				//演算子が押されたら、文字列をboxへ格納し、文字列を初期化。演算子はsubBoxへ格納
 				//subBoxへ格納された演算子が、直前にsubBoxへ格納された演算子より優先順位が低ければ、直前の演算子をboxへ格納
 				//box内に演算子が格納されたとき、文字列を最後から3つ取り出して計算し、boxへ戻す
-				if(12 <= i && i <= 15) {
+				else if(12 <= i && i <= 15) {
 					if(str != "") {//strが空でない必要がある
 						box.add(str);
-					}
-					str = "";
-					subBox.add(button[i].getText());
-					if(!subBox.isEmpty()) {
-						if((i ==12 || i ==13) && (subBox.get(subBox.size()-2) == "×" || subBox.get(subBox.size()-2) == "÷")) {
-							box.add(subBox.get(subBox.size()-2));
-							subBox.remove(subBox.size()-2);
-							double d1 = Double.parseDouble(box.get(box.size()-3));
-							double d2 = Double.parseDouble(box.get(box.size()-1));
-							String s = box.get(box.size()-1);
-							operand = calc(d1, d2, s);
+						str = ""; //文字列初期化
+						button[10].setEnabled(true); //小数点ボタン有効化
+						subBox.add(button[i].getText());
+						if(subBox.size() > 1) { //subBoxに二つ以上の演算子が格納されている場合
+							//+または-を押した場合かつ直前の演算子が×または÷の場合
+							if((i ==12 || i ==13) && (subBox.get(subBox.size()-2) == "×" || subBox.get(subBox.size()-2) == "÷")) {
+								box.add(subBox.get(subBox.size()-2));
+								subBox.remove(subBox.size()-2);
+								d1 = Double.parseDouble(box.get(box.size()-3));
+								d2 = Double.parseDouble(box.get(box.size()-2));
+								String s = box.get(box.size()-1);
+								box.remove(box.size()-1);
+								box.remove(box.size()-1);
+								box.remove(box.size()-1);
+								System.out.println("d1は" + d1 + "、d2は" + d2 + "、sは" + s);
+								operand = calc(d1, d2, s);
+								System.out.println("operandは" + operand);
+								str = String.valueOf(operand);
+								box.add(str);
+								str = "";
+								//subBoxに+または-の演算子が二つ並んだ場合
+								if(subBox.size() > 1) {
+									if((subBox.get(subBox.size()-2) == "+" || subBox.get(subBox.size()-2) == "-") &&
+										(subBox.get(subBox.size()-1) == "+" || subBox.get(subBox.size()-1) == "-")) {
+										box.add(subBox.get(subBox.size()-2));
+										subBox.remove(subBox.size()-2);
+										d1 = Double.parseDouble(box.get(box.size()-3));
+										d2 = Double.parseDouble(box.get(box.size()-2));
+										s = box.get(box.size()-1);
+										box.remove(box.size()-1);
+										box.remove(box.size()-1);
+										box.remove(box.size()-1);
+										System.out.println("d1は" + d1 + "、d2は" + d2 + "、sは" + s);
+										operand = calc(d1, d2, s);
+										System.out.println("operandは" + operand);
+										str = String.valueOf(operand);
+										box.add(str);
+										str = "";
+									}
+								}
+							}
+							//+または-を押した場合かつ直前の演算子が+または-の場合
+							else if((i ==12 || i ==13) && (subBox.get(subBox.size()-2) == "+" || subBox.get(subBox.size()-2) == "-")) {
+								box.add(subBox.get(subBox.size()-2));
+								subBox.remove(subBox.size()-2);
+								d1 = Double.parseDouble(box.get(box.size()-3));
+								d2 = Double.parseDouble(box.get(box.size()-2));
+								String s = box.get(box.size()-1);
+								box.remove(box.size()-1);
+								box.remove(box.size()-1);
+								box.remove(box.size()-1);
+								System.out.println("d1は" + d1 + "、d2は" + d2 + "、sは" + s);
+								operand = calc(d1, d2, s);
+								System.out.println("operandは" + operand);
+								str = String.valueOf(operand);
+								box.add(str);
+								str = "";
+							}
+							//×または÷を押した場合かつ直前の演算子が×または÷の場合
+							else if((i ==14 || i ==15) && (subBox.get(subBox.size()-2) == "×" || subBox.get(subBox.size()-2) == "÷")) {
+								box.add(subBox.get(subBox.size()-2));
+								subBox.remove(subBox.size()-2);
+								d1 = Double.parseDouble(box.get(box.size()-3));
+								d2 = Double.parseDouble(box.get(box.size()-2));
+								s = box.get(box.size()-1);
+								box.remove(box.size()-1);
+								box.remove(box.size()-1);
+								box.remove(box.size()-1);
+								System.out.println("d1は" + d1 + "、d2は" + d2 + "、sは" + s);
+								operand = calc(d1, d2, s);
+								System.out.println("operandは" + operand);
+								str = String.valueOf(operand);
+								box.add(str);
+								str = "";
+								//subBoxに+または-の演算子が二つ並んだ場合
+								if(subBox.size() > 1) {
+									if((subBox.get(subBox.size()-2) == "+" || subBox.get(subBox.size()-2) == "-") &&
+										(subBox.get(subBox.size()-1) == "+" || subBox.get(subBox.size()-1) == "-")) {
+										box.add(subBox.get(subBox.size()-2));
+										subBox.remove(subBox.size()-2);
+										d1 = Double.parseDouble(box.get(box.size()-3));
+										d2 = Double.parseDouble(box.get(box.size()-2));
+										s = box.get(box.size()-1);
+										box.remove(box.size()-1);
+										box.remove(box.size()-1);
+										box.remove(box.size()-1);
+										System.out.println("d1は" + d1 + "、d2は" + d2 + "、sは" + s);
+										operand = calc(d1, d2, s);
+										System.out.println("operandは" + operand);
+										str = String.valueOf(operand);
+										box.add(str);
+										str = "";
+									}
+								}
+							}
 						}
 					}
+					
 				}
-				
 			}	
+		}
+		if(box.size() > 0) {
+			for(int j = 0; j < box.size(); j++) {
+				System.out.println("boxの" + j + "番目は" + box.get(j));
+			}
+		}
+		if(subBox.size() > 0) {
+			for(int j = 0; j < subBox.size(); j++) {
+				System.out.println("subBoxの" + j + "番目は" + subBox.get(j));
+			}
 		}
 	}
 	
@@ -129,16 +256,20 @@ class CalcFrame extends JFrame implements ActionListener{
 		double d = 0;
 		switch(s) {
 		case "+":
+			System.out.println("case+が実行されました");
 			d =  d1 + d2;
 			break;
 		case "-":
-			d = d1 + d2;
+			System.out.println("case-が実行されました");
+			d = d1 - d2;
 			break;
 		case "×":
-			d = d1 + d2;
+			System.out.println("case×が実行されました");
+			d = d1 * d2;
 			break;
 		case "÷":
-			d = d1 + d2;
+			System.out.println("case÷が実行されました");
+			d = d1 / d2;
 			break;
 		}
 		return d;
